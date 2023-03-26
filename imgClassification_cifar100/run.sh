ee8a292e4b2a8f22ad86a76801793b747ca9b2a9
Guoqiang Zhang <guoqiang.x.zhang@gmail.com>
2023-03-27 01:43:58 +0200

    add cifar100

---
+++ imgClassification_cifar100/run.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=0 python main.py --optim aida --lr 1e-3 --eps 1e-9 --beta1 0.9 --beta2 0.999 --momentum 0.9 --model vgg --run 0
+
+
+CUDA_VISIBLE_DEVICES=0 python main.py --optim aida --lr 1e-3 --eps 1e-9 --beta1 0.9 --beta2 0.999 --momentum 0.9 --model resnet --run 0
